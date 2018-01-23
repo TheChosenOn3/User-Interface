5e68248004a94415cb1ca0dfaebd522b1ddad9a6
EKilian <EKilian@EKIL-NB-01>
2018-01-23 14:29:41 +0200

    het al die formms gemaak

---
+++ Project500/Project500/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project500
+{
+    public partial class Register : MetroFramework.Forms.MetroForm
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project500/Project500/Schedules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project500
+{
+    public partial class Schedules : MetroFramework.Forms.MetroForm
+    {
+        public Schedules()
+        {
+            InitializeComponent();
+        }
+
+        private void Schedules_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
