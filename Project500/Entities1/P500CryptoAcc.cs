e04f6821d884dcda3baf177d2a059fdcc0cbecf4
EKilian <EKilian@EKIL-NB-01>
2018-01-28 13:43:38 +0200

    vlaccbt

---
+++ Project500/Entities1/P500CryptoAcc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities1
+{
+    class P500CryptoAcc
+    {
+        string Waletname = "BTCACC";
+        float Waletammount = 1234;
+        string WalletID = "234567890";
+
+        public string Waletname1 { get => Waletname; set => Waletname = value; }
+        public float Waletammount1 { get => Waletammount; set => Waletammount = value; }
+        public string WalletID1 { get => WalletID; set => WalletID = value; }
+        public P500CryptoAcc(string Waletname1, float Waletammount1, string WalletID1)
+        {
+            this.WalletID = WalletID1;
+            this.Waletammount = Waletammount1;
+            this.Waletname = Waletname1;
+        }
+        public P500CryptoAcc()
+        {
+
+        }
+    
+
+    }
+}
