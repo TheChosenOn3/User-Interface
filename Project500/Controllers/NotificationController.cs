bf566ff736d4ac8022ef9dbc1b84cef46c05293e
Jason du Plessis <jasondp6@hotmail.com>
2018-01-30 21:45:17 +0200

    ADDED sHIT

---
+++ Project500/Controllers/NotificationController.cs
@@ -0,0 +1,15 @@
+﻿using Entities1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controllers
+{
+   public class NotificationController
+    {
+        //public static List<Notification> getNotifications(string UserID)
+        //{
+            
+        //}
+    }
+}
