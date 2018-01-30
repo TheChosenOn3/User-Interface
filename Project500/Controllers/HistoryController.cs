dee6fb2f473b3cc6ce3832d87621609e803c9f68
DTerblanche <DTerblanche@DTER-NB-01>
2018-01-30 23:19:51 +0200

    History Controller Added

---
+++ Project500/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+﻿using Entities1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controllers
+{
+   public  class HistoryController
+    {
+        static string Control = "History/";
+        public static List<History> getHistory(string id)
+        {
+            return ControllerHandler<History>.Search(Control + id);
+        }
+    }
+}
